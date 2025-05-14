111ac493427502e9e56bff669fe7c5cb6cc5cee6
ismail-ssss <ibhattipuri@gmail.com>
2025-05-14 10:46:39 +0530

    update client

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -7,17 +7,7 @@ function Home({movies}) {
     <div>
       <h1>home page</h1>
       <div className="container-fluid">
-        <div className="row border">
-
-          <div className="col-5 border p-3">
-            {movies.map((movie,id)=>(
-              <Movie key={id} data={movie}></Movie>
-            ))}
-          </div>
-          <div className="col-5 mt-5 m-auto">
-            <Outlet></Outlet>
-          </div>
-        </div>
+        <h1>home</h1>
       </div>
     </div>
   );
